9b6945efd48cd2cb778c248f4d41f967e1197605
Ryan Hoffman <rhoffman@copia.io>
2022-04-14 17:42:48 -0700

    weight design 1 working

--- consensus_filter_1.py
+++ consensus_filter_1.py
@@ -1,7 +1,8 @@
+import copy
 import matplotlib.pyplot as plt
 import numpy as np
 
-ITERATIONS = 10
+ITERATIONS = 2000
 NUM_NODES = 10
 DIMENSION = 2
 MAX_DISTANCE = 4
@@ -61,7 +62,8 @@ def getNetwork():
         node = network[name]
         for nei in node['nei']:
             plt.plot([node['pos'][0], network[nei]['pos'][0]], [node['pos'][1], network[nei]['pos'][1]])
-    plt.savefig('test.png')
+    plt.savefig('network.png')
+    plt.clf()
 
     #   Calculate average position of all nodes on network
     avgPos = np.sum([network[name]['pos'] for name in network], axis=0) / NUM_NODES
@@ -70,20 +72,14 @@ def getNetwork():
     return network, avgPos
 
 network, avgPos = getNetwork()
-nodes_va = 50 * np.ones((NUM_NODES,1)) + 1 * np.random.randn(NUM_NODES, 1)
-nodes_va_old = nodes_va
-n1 = [0] * NUM_NODES
-m1 = [0] * NUM_NODES
-x
+x_i = F1 * np.ones((NUM_NODES,1)) + 1 * np.random.randn(NUM_NODES, 1)
+x_i_initial = copy.copy(x_i)
+x_i_old = copy.copy(x_i)
 
 weights = {str(i): {str(j): 0 for j in network} for i in network}
 
 def v_i(pos, node):
     v1 = ((np.linalg.norm(pos - avgPos)**2) + c_v) / (COMMUNICATION_RADIUS**2)
-    
-    n1[node] = np.random.normal(0.0, v1)
-
-    m1[node] = F1 + n1[node]
 
     return v1
 
@@ -93,14 +89,27 @@ for i in range(1, ITERATIONS):
         for nei in network[name]['nei']:
             weights[name][nei] = c_lw / (v_i(network[name]['pos'], int(name)) + v_i(network[nei]['pos'], int(name)))
             sigma_weight += weights[name][nei]
-        weights[name, name] = 1 - sigma_weight
+        weights[name][name] = 1 - sigma_weight
 
         addition = 0
         for nei in network[name]['nei']:
-            addition += weights[name][nei] * nodes_va_old[int(nei)]
+            addition += weights[name][nei] * x_i_old[int(nei)]
         
-        nodes_va[int(name)] = weights[name][name] * nodes_va_old[int(name)] + addition
+        x_i[int(name)] = weights[name][name] * x_i_old[int(name)] + addition
     
-    nodes_va_old = nodes_va
+    x_i_old = copy.copy(x_i)
 
 
+#   Plot measurements
+measurement_x_initial = [i+1 for i in range(NUM_NODES)]
+measurement_y_initial = x_i_initial
+
+measurement_x_final = [i+1 for i in range(NUM_NODES)]
+measurement_y_final = x_i
+
+plt.plot(measurement_x_initial, measurement_y_initial, label="Initial Measurement")
+plt.plot(measurement_x_final, measurement_y_final, label="Final Measurement")
+plt.xlabel("Node")
+plt.ylabel("Measurement Value")
+plt.savefig("measurements.png")
+plt.clf()
